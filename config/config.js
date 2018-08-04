86337009140c9d1c92c8392d806d8810d86754bf
GeoTechVision <geotechvision.dev@gmail.com>
2018-08-04 02:52:32 -0500

    added sandbox db

---
+++ config/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    token: '9Z7Tu5BDRKEUdFbu2Vg8WclNHLQT7meb',
+    recaptch:{
+      secretKey:{
+        local:'6Lcq5kMUAAAAAKsZ_vGOfQXE8Wl_0ZOYq2fH4DOj',
+        web:'6LcBk08UAAAAAGFVVRdN1A7hdsoHSjWn42D1teOK'
+      },
+      siteKey:{
+        local:'6Lcq5kMUAAAAAPg4nyraF2m4RAxVbyamuEduWy57',
+        web:'6LcBk08UAAAAAKQujGBb9GZ13nt0AOiJWLTOyeaC'
+      }
+    },
+    nodeMailer:{
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      username:'geotechvision@gmail.com',
+      pass:'Matt2414fulfill'
+    }
+  }
+
---
+++ config/database.js
@@ -0,0 +1,9 @@
+//database connection parameters please see http://docs.mlab.com/ for more info
+let db_vendor = '@ds113122.mlab.com:13122/notivize_db'
+let db_user = 'notivize_sandbox';
+let db_password = 'sandman1';
+
+module.exports = {
+  database: 'mongodb://' + db_user + ':' + db_password + db_vendor,
+  apiKey: '632422c8282d40ef988c927ab3790836'
+}

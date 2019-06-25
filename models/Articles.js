e22b84c075858e8f11fbe8c0cb94f3f46377d978
Sheldon Small <ssmall@realdecoy.com>
2019-06-25 16:42:38 -0500

    updated article model with beautify

--- models/Articles.js
+++ models/Articles.js
@@ -2,61 +2,63 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // Articles Schema
 let ArticlesSchema = new Schema({
-	title:{
-		type:String,
-		unique:true,
-		required:true
+	title: {
+		type: String,
+		unique: true,
+		required: true
 	},
-	urlTitle:{
-    type:String,
-		required:false
+	urlTitle: {
+		type: String,
+		required: false
 	},
-	subject:{
-		type:String,
-		required:false
+	subject: {
+		type: String,
+		required: false
 	},
-	description:{
-			type:String,
-			required:false
+	description: {
+		type: String,
+		required: false
 	},
-	author:{
-		type:String,
-		required:false
+	author: {
+		type: String,
+		required: false
 	},
-	source:{
-    type:String,
-		required:true
+	source: {
+		type: String,
+		required: true
 	},
-	category:{
-		type:String,
-		required:true
+	category: {
+		type: String,
+		required: true
 	},
-	publishedAt:{
-    type:String,
-		required:true
+	publishedAt: {
+		type: String,
+		required: true
 	},
-	publishedDate:{
-		type:Date,
-		required:false
+	publishedDate: {
+		type: Date,
+		required: false
 	},
-	timeStamp:{
-		type:Date,
-		required:false,
+	timeStamp: {
+		type: Date,
+		required: false,
 		default: Date.now
 	},
-	urlToImage:{
-    type:String,
-		required:false
+	urlToImage: {
+		type: String,
+		required: false
 	},
-	url:{
-    type:String,
-		required:true
+	url: {
+		type: String,
+		required: true
 	},
 	mview: {
-			type:String,
-			required:false,
-			default:""
+		type: String,
+		required: false,
+		default: ""
 	}
-}, { collection : 'articles' });
+}, {
+	collection: 'articles'
+});
 
 const Articles = module.exports = mongoose.model('Articles', ArticlesSchema);

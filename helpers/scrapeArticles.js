a181484693125ff455c7297a1effad88dc1e8031
Sheldon Small <sheldonsmall.io@gmail.com>
2020-10-09 15:38:14 -0500

    created scrape article helper methods that bring the utility methods and article creation together

---
+++ helpers/scrapeArticles.js
@@ -0,0 +1,30 @@
+const moment = require('moment');
+const { NEWS_SOURCE } = require('../config/news-config');
+const { getNewsHeadline, scrapeArticleDetail } = require('../utilities/scrapeIntNews');
+const { saveArticles } = require('../controllers/ArticlesController');
+const config = require('../config/env');
+const apiKey = config.apikeys.newsAPI;
+
+module.exports.scrapeIntNews = async () => {
+    for (let i in NEWS_SOURCE) {
+        let elem = NEWS_SOURCE[i]
+        headline = await getNewsHeadline(`${elem.url}${apiKey}`);
+        let articleDetails  = await scrapeArticleDetail(headline.url, elem.html_scraper_class);
+        await saveArticles({
+            title: headlin.title,
+            urlTitle: headlin.title ? (headlin.title).replace(/[\. ,:-]+/g, "-"): '',
+            author: headline.author,
+            subject: headline.description,
+            content: headline.content,
+            source: elem.source,
+            category: elem.category,
+            publishedAt: headline.publishedAt,
+            publishedDate: moment(headline.publishedAt).format("MMM Do YY"),
+            description: articleDetails,
+            urlToImage: headline.UrlToImage,
+            url: headline.url
+        });
+    }
+
+
+}

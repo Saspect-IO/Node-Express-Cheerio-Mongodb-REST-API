205d9b863b48f900901227adf0a23162f4c76b87
Sheldon Small <sheldonsmall.io@gmail.com>
2020-10-10 12:46:50 -0500

    handle request error, log message to console

--- utilities/scrapeIntNews.js
+++ utilities/scrapeIntNews.js
@@ -3,17 +3,20 @@ const cheerio = require('cheerio');
 
 // scrapes news details from each news article url by <p> tags setup the article properties and push to Articles Object that mimics the news API JSON Structure.
 module.exports.scrapeArticleDetail = async (artilceUrl, htmlScraperClass) => {
-
-  const response = await axios.get(artilceUrl)
-
-  let $ = cheerio.load(response.data);
-  let articleDetails = null;
-  $(htmlScraperClass).each((i, elm) => {
-    let data = $(elm);
-    let addedSpace = data.find("p").text();
-    articleDetails = addedSpace.replace(/(.*?\.){3}/g, '$& \n\n');
-  });
-  return articleDetails;
+  try {
+    const response = await axios.get(artilceUrl);
+    let $ = cheerio.load(response.data);
+    let articleDetails = null;
+    $(htmlScraperClass).each((i, elm) => {
+      let data = $(elm);
+      let addedSpace = data.find("p").text();
+      articleDetails = addedSpace.replace(/(.*?\.){3}/g, '$& \n\n');
+    });
+    
+    return articleDetails;
+  } catch (err) {
+    console.log('[Error]:', err.message);
+  }
 }
 
 //fetch json data from news api for each url.
@@ -23,6 +26,6 @@ module.exports.getNewsHeadline = async (url) => {
     let newsArticle = resp.data.articles;
     return newsArticle
   } catch (err) {
-    console.error(err);
+    console.log('[Error]:', err.message);
   }
 }

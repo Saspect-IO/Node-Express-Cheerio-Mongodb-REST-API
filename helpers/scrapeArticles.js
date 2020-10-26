bb6f853d3201b293e8fa0bf462348c1a9f2f7215
Sheldon Small <sheldonsmall.io@gmail.com>
2020-10-25 19:52:12 -0500

    add null checks and import news apikey from env configs

--- helpers/scrapeArticles.js
+++ helpers/scrapeArticles.js
@@ -2,26 +2,27 @@ const moment = require('moment');
 const { NEWS_SOURCE } = require('../config/news-config');
 const { getNewsHeadline, scrapeArticleDetail } = require('../utilities/scrapeIntNews');
 const { saveArticles } = require('../controllers/ArticlesController');
+const config = require('../config/env')
 
 module.exports.scrapeIntNews = async () => {
     for (let i in NEWS_SOURCE) {
         let elem = NEWS_SOURCE[i];
-        let headlines = await getNewsHeadline(`${elem.url}${apiKey}`);
+        let headlines = await getNewsHeadline(`${elem.url}${config.apikeys.newsAPI}`);
         for (const key in headlines) {
-            let articleDetails = await scrapeArticleDetail(headlines[key].url, elem.html_scraper_class);
+            let articleDetails = await scrapeArticleDetail(headlines[key] ? headlines[key].url : "", elem.html_scraper_class);
             await saveArticles({
-                title: headlines[key].title,
+                title: headlines[key] ? headlines[key].title : "",
                 urlTitle: headlines[key].title ? (headlines[key].title).replace(/[\. ,:-]+/g, "-") : '',
-                author: headlines[key].author,
-                subject: headlines[key].description,
-                content: headlines[key].content,
+                author: headlines[key] ? headlines[key].author : "",
+                subject: headlines[key] ? headlines[key].description : "",
+                content: headlines[key] ? headlines[key].content : "",
                 source: elem.source,
                 category: elem.category,
                 publishedAt: headlines[key] ? headlines[key].publishedAt ? headlines[key].publishedAt : '' : '',
                 publishedDate: headlines[key] ? headlines[key].publishedAt ? moment(headlines[key].publishedAt).format() : moment().format() : moment().format(),
                 description: articleDetails,
-                urlToImage: headlines[key].UrlToImage,
-                url: headlines[key].url
+                urlToImage: headlines[key] ? headlines[key].UrlToImage : "",
+                url: headlines[key] ? headlines[key].url  : "",
             });
         }
     }
